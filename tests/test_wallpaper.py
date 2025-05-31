70fecb1a4a23f6b8e44297e4f573a10509e62b73
Albert Hopkins <marduk@letterboxes.org>
2025-05-31 12:55:58 -0500

    tests: add a test

--- tests/test_wallpaper.py
+++ tests/test_wallpaper.py
@@ -1,8 +1,28 @@
 # pylint: disable=missing-docstring
+import configparser
 import subprocess as sp
+from pathlib import Path
 from unittest import TestCase, mock
 
+from larry.color import ColorList
+from larry.config import ConfigType
+
 from larry4mac import wallpaper
+
+
+@mock.patch.object(wallpaper, "set_wallpaper")
+class PluginTests(TestCase):
+    def test(self, set_wallpaper: mock.Mock) -> None:
+        colors: ColorList = []
+        parser = configparser.ConfigParser()
+        parser.add_section("larry4mac")
+        config = ConfigType(parser, name="larry4mac")
+        config["input"] = "/dev/null"
+
+        wallpaper.plugin(colors, config)
+
+        self.assertEqual(2, set_wallpaper.call_count)
+        set_wallpaper.assert_called_with(Path("/dev/null"))
 
 
 @mock.patch.object(wallpaper.subprocess, "Popen")
